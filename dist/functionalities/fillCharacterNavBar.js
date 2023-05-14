a36bf18891ddaf63ed4bf3cce45117755ac352a4
Matias Alaimo <matias.chiappa@hotmail.com>
2023-05-15 00:38:46 +0200

    Optimized: changed fetch in FillCharNavBar to be more optimal

--- dist/functionalities/fillCharacterNavBar.js
+++ dist/functionalities/fillCharacterNavBar.js
@@ -8,52 +8,56 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 export function fillCharacterNavBar() {
-    const navCharactersContainer = document.querySelector("#navCharactersContainer");
-    let dataNumber = 0;
-    for (let i = 1; i < 43; i++) {
-        fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-            .then(response => response.json())
-            .then(data => {
-            data.results.forEach((character) => {
-                dataNumber++;
-                const button = document.createElement("button");
-                button.textContent = character.name;
-                button.setAttribute("type", "button");
-                button.setAttribute("data-id", dataNumber.toString());
-                button === null || button === void 0 ? void 0 : button.addEventListener("click", (event) => __awaiter(this, void 0, void 0, function* () {
-                    cleanEpisodeBoard();
-                    const target = event.currentTarget;
-                    const id = target.getAttribute("data-id");
-                    console.log(id);
-                    if (id) {
-                        const character = yield fetchCharacter(id);
-                        const characterImg = document.querySelector("#characterImg");
-                        characterImg === null || characterImg === void 0 ? void 0 : characterImg.setAttribute("src", character.image);
-                        const charName = document.querySelector("#charName");
-                        charName.innerText = character.name;
-                        const charStatus = document.querySelector("#charStatus");
-                        charStatus.innerText = character.status;
-                        const charSpecie = document.querySelector("#charSpecie");
-                        charSpecie.innerText = character.species;
-                        const charGender = document.querySelector("#charGender");
-                        charGender.innerText = character.gender;
-                        const charOrigin = document.querySelector("#charOrigin");
-                        charOrigin.innerText = character.origin;
-                        const episodeListContainer = document.querySelector("#episodeListContainer");
-                        const episodes = character.episode;
-                        episodes.forEach((element) => {
-                            const episodeNumber = element.replace(/\D/g, "");
-                            const episodeElement = document.createElement("p");
-                            episodeElement.setAttribute("id", "episodeElement");
-                            episodeElement.innerText = `Episode ${episodeNumber}`;
-                            episodeListContainer.appendChild(episodeElement);
-                        });
-                    }
-                }));
-                navCharactersContainer.appendChild(button);
-            });
-        });
-    }
+    return __awaiter(this, void 0, void 0, function* () {
+        const navCharactersContainer = document.querySelector("#navCharactersContainer");
+        let dataNumber = 0;
+        const fetchPromises = [];
+        for (let i = 1; i < 43; i++) {
+            fetchPromises.push(fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+                .then(response => response.json())
+                .then(data => {
+                data.results.forEach((character) => {
+                    dataNumber++;
+                    const button = document.createElement("button");
+                    button.textContent = character.name;
+                    button.setAttribute("type", "button");
+                    button.setAttribute("data-id", dataNumber.toString());
+                    button === null || button === void 0 ? void 0 : button.addEventListener("click", (event) => __awaiter(this, void 0, void 0, function* () {
+                        cleanEpisodeBoard();
+                        const target = event.currentTarget;
+                        const id = target.getAttribute("data-id");
+                        console.log(id);
+                        if (id) {
+                            const character = yield fetchCharacter(id);
+                            const characterImg = document.querySelector("#characterImg");
+                            characterImg === null || characterImg === void 0 ? void 0 : characterImg.setAttribute("src", character.image);
+                            const charName = document.querySelector("#charName");
+                            charName.innerText = character.name;
+                            const charStatus = document.querySelector("#charStatus");
+                            charStatus.innerText = character.status;
+                            const charSpecie = document.querySelector("#charSpecie");
+                            charSpecie.innerText = character.species;
+                            const charGender = document.querySelector("#charGender");
+                            charGender.innerText = character.gender;
+                            const charOrigin = document.querySelector("#charOrigin");
+                            charOrigin.innerText = character.origin;
+                            const episodeListContainer = document.querySelector("#episodeListContainer");
+                            const episodes = character.episode;
+                            episodes.forEach((element) => {
+                                const episodeNumber = element.replace(/\D/g, "");
+                                const episodeElement = document.createElement("p");
+                                episodeElement.setAttribute("id", "episodeElement");
+                                episodeElement.innerText = `Episode ${episodeNumber}`;
+                                episodeListContainer.appendChild(episodeElement);
+                            });
+                        }
+                    }));
+                    navCharactersContainer.appendChild(button);
+                });
+            }));
+        }
+        yield Promise.all(fetchPromises);
+    });
 }
 var CharacterStatus;
 (function (CharacterStatus) {
--- src/functionalities/fillCharacterNavBar.ts
+++ src/functionalities/fillCharacterNavBar.ts
@@ -1,59 +1,64 @@
-export function fillCharacterNavBar(): void{
+export async function fillCharacterNavBar(): Promise<void> {
     const navCharactersContainer: HTMLButtonElement | null = document.querySelector("#navCharactersContainer");
     let dataNumber: number = 0;
-    for (let i = 1; i < 43; i++){
-    fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-        .then(response => response.json())
-        .then(data => {
+    const fetchPromises: Promise<void>[] = [];
+
+    for (let i = 1; i < 43; i++) {
+      fetchPromises.push(
+        fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+          .then(response => response.json())
+          .then(data => {
             data.results.forEach((character: { name: string }) => {
-                dataNumber++;
-                const button = document.createElement("button");
-                button.textContent = character.name;
-                button.setAttribute("type", "button");
-                button.setAttribute("data-id", dataNumber.toString());
-                button?.addEventListener("click", async (event: MouseEvent) => {
-                    cleanEpisodeBoard();
-                    const target = event.currentTarget as HTMLElement;
-                    const id = target.getAttribute("data-id")
-                    console.log(id);
-                    if(id){
-                    const character = await fetchCharacter(id);
-                    const characterImg = document.querySelector("#characterImg");
-                    characterImg?.setAttribute("src", character.image);
+              dataNumber++;
+              const button = document.createElement("button");
+              button.textContent = character.name;
+              button.setAttribute("type", "button");
+              button.setAttribute("data-id", dataNumber.toString());
+              button?.addEventListener("click", async (event: MouseEvent) => {
+                cleanEpisodeBoard();
+                const target = event.currentTarget as HTMLElement;
+                const id = target.getAttribute("data-id");
+                console.log(id);
+                if (id) {
+                  const character = await fetchCharacter(id);
+                  const characterImg = document.querySelector("#characterImg");
+                  characterImg?.setAttribute("src", character.image);
 
-                    const charName = document.querySelector("#charName") as HTMLElement;
-                    charName.innerText = character.name;
+                  const charName = document.querySelector("#charName") as HTMLElement;
+                  charName.innerText = character.name;
 
-                    const charStatus = document.querySelector("#charStatus") as HTMLElement;
-                    charStatus.innerText = character.status;
+                  const charStatus = document.querySelector("#charStatus") as HTMLElement;
+                  charStatus.innerText = character.status;
 
-                    const charSpecie = document.querySelector("#charSpecie") as HTMLElement;
-                    charSpecie.innerText = character.species;
+                  const charSpecie = document.querySelector("#charSpecie") as HTMLElement;
+                  charSpecie.innerText = character.species;
 
-                    const charGender = document.querySelector("#charGender") as HTMLElement;
-                    charGender.innerText = character.gender;
+                  const charGender = document.querySelector("#charGender") as HTMLElement;
+                  charGender.innerText = character.gender;
 
-                    const charOrigin = document.querySelector("#charOrigin") as HTMLElement;
-                    charOrigin.innerText = character.origin;
+                  const charOrigin = document.querySelector("#charOrigin") as HTMLElement;
+                  charOrigin.innerText = character.origin;
 
-                    const episodeListContainer = document.querySelector("#episodeListContainer") as HTMLElement;
-                    const episodes = character.episode;
-                    episodes.forEach((element: string) => {
-                        const episodeNumber: string = element.replace(/\D/g, "");
-                        const episodeElement = document.createElement("p");
-                        episodeElement.setAttribute("id", "episodeElement");
-                        episodeElement.innerText = `Episode ${episodeNumber}`;
-                        episodeListContainer.appendChild(episodeElement);
-                    })
+                  const episodeListContainer = document.querySelector("#episodeListContainer") as HTMLElement;
+                  const episodes = character.episode;
+                  episodes.forEach((element: string) => {
+                    const episodeNumber: string = element.replace(/\D/g, "");
+                    const episodeElement = document.createElement("p");
+                    episodeElement.setAttribute("id", "episodeElement");
+                    episodeElement.innerText = `Episode ${episodeNumber}`;
+                    episodeListContainer.appendChild(episodeElement);
+                  });
+                }
+              });
 
-                    }
-                })
-
-                navCharactersContainer!.appendChild(button);
-            })
-        })
+              navCharactersContainer!.appendChild(button);
+            });
+          })
+      );
     }
-}
+
+    await Promise.all(fetchPromises);
+  }
 
 interface Character {
     name: string;
